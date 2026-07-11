576ac3e66ea6c8fa415279e31e2e881a0063df43
wad-sh <tawjehiwadee@gmail.com>
2026-07-12 00:44:05 +0300

    Last endpoints added

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import *
-from enum import Enum
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,10 +12,10 @@ users =[]
 @app.post("/users")
 def adduser(user: Usr) :
     if user.age < 0:
-        return {
-            "message": "Error: Invalid age.",
-            "user": None
-        }
+        raise HTTPException(
+    status_code=400,
+    detail="Invalid age"
+)
     x = Gid()
     users.append({
         "id":x,
@@ -44,12 +43,72 @@ def allusers () :
     return users
 
 @app.get("/users/{id}")
-def getuser (uid: int) :
+def getuser (id: int) :
     for user in users :
-        if user["id"] == uid :
+        if user["id"] == id :
             return user
     raise HTTPException(
-        status_code= 404
+        status_code= 404,
         detail= "User not found"
     )
 
+@app.delete("/users/{id}")
+def deleteuser (id: int):
+    for user in users :
+        if user["id"] == id :
+            users.remove(user)
+            return {
+                "message":"User deleted",
+                "id" : id
+                }
+    raise HTTPException(
+        status_code= 404,
+        detail= "User not found"
+    )
+
+@app.put("/users/{id}")
+def updateuser (id: int, user: Usr) :
+    if user.age < 0:
+        raise HTTPException(
+    status_code=400,
+    detail="Invalid age"
+)
+    
+    for u in users :
+        if u["id"] == id :
+            u["name"] = user.name
+            u["age"] = user.age
+            return {
+                "message":"User updated",
+                "user" : u
+                }
+    raise HTTPException(
+        status_code= 404,
+        detail= "User not found"
+    )
+
+class Userup (BaseModel):
+    name: str | None =None
+    age: int | None = None
+
+@app.patch("/users/{id}")
+def updateusernameorage (id: int, user: Userup) :
+    if user.age is not None and user.age < 0:
+        raise HTTPException(
+    status_code=400,
+    detail="Invalid age"
+)
+    for u in users :
+        if u["id"] == id :
+            if user.name is not None :
+                u["name"] = user.name
+            if user.age is not None :
+                u["age"] = user.age
+            return {
+                "message":"User updated",
+                "user" : u
+                }
+    raise HTTPException(
+        status_code= 404,
+        detail= "User not found"
+    )
